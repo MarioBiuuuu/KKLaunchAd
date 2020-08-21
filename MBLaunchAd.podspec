ce948ad36ade19969f0e720309de7a86c328d7dc
adrian <adrian@gpower.co>
2020-08-21 20:28:34 +0800

    fix white bg color bug

--- MBLaunchAd.podspec
+++ MBLaunchAd.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MBLaunchAd'
-  s.version          = '1.2.2'
+  s.version          = '1.2.3'
   s.summary          = 'A short description of KKLaunchAd.'
 
 # This description is used to generate tags and improve search results.
--- MBLaunchAd/MBLaunchAd/MBLaunchImageView.m
+++ MBLaunchAd/MBLaunchAd/MBLaunchImageView.m
@@ -15,7 +15,7 @@
     if (self) {
         self.frame = [UIScreen mainScreen].bounds;
         self.userInteractionEnabled = YES;
-        self.backgroundColor = [UIColor whiteColor];
+        self.backgroundColor = [UIColor clearColor];
         switch (sourceType) {
             case SourceTypeLaunchImage:{
                 self.image = [self imageFromLaunchImage];
